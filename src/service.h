29cffdd62c1f8c00afdbbebad4d1da5af2713ea8
deskjet <simon.guendling@gmx.de>
2013-06-30 12:30:07 +0200

    removing http server

--- src/service.h
+++ src/service.h
@@ -8,16 +8,12 @@
 
 namespace botscript_server {
 
-/// This service starts an HTTP server serving files that were "compiled" in
-/// the contents.h / contents.cpp file.
-/// Additionally it handles Web Socket connections enabling to control
+/// This service handles Web Socket connections enabling to control
 /// (create/delete/configure) bots.
 class service {
  public:
   /// Initializes the io_service with NULL.
-  service() : io_service_(nullptr) {
-    std::cerr << "service::service()\n";
-  }
+  service();
 
   /// Starts the server.
   ///
@@ -25,23 +21,10 @@ class service {
   /// \param http_port the HTTP port to listen on
   void start(short ws_port);
 
-  /// Stops the service again.
+  /// Stops the service.
   void stop();
 
  private:
-  /// Utility function to calculate the SHA1 checksum in HEX format.
-  ///
-  /// \param data   the data to hash
-  /// \param count  the amount of data to hash
-  /// \return the SHA1 checksum
-  static std::string sha1sum(const void *data, std::size_t count);
-
-  /// URL decodes the given string and writes the decoded string to out
-  ///
-  /// \param in   the in string to decode
-  /// \param out  the decoded string
-  /// \return whether the decoding was successful
-  static bool url_decode(const std::string& in, std::string& out);
 
   /// Points to the io_service currently in use.
   boost::asio::io_service* io_service_;
