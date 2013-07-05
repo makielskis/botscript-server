184fa76e60154d48b6fb318f5a7388620d227031
Felix Gündling <felixguendling@gmail.com>
2013-07-05 15:05:53 +0200

    qt5

--- src/qtgui/main.cpp
+++ src/qtgui/main.cpp
@@ -3,18 +3,24 @@
 // https://raw.github.com/makielski/botscript/master/COPYING
 
 #include <QtGui>
+#include <QApplication>
 
 #include "./Window.h"
 #include "./BotThread.h"
+
+class QApplication;
 
 int main(int argc, char *argv[]) {
   Q_INIT_RESOURCE(systray);
 
   QApplication app(argc, argv);
+  app.setOrganizationName("Makielski");
+  app.setApplicationName("Bot Script");
   QApplication::setQuitOnLastWindowClosed(false);
 
   BotThread server;
   server.start();
+  std::cout << "starting server ..\n";
 
   Window window(server);
 
