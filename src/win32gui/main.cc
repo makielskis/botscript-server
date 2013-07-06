1bd72514afba47c19161d33ecea168380552a78b
Felix Gündling <felix.guendling@gmail.com>
2013-07-06 12:01:30 +0200

    win32 gui cleanup

--- src/win32gui/main.cc
+++ src/win32gui/main.cc
@@ -13,66 +13,65 @@ botscript_server::service s;
 std::thread t;
 
 void on_action(CTrayIcon* pTrayIcon, UINT uMsg) {
-	if (uMsg != WM_RBUTTONUP) {
+  if (uMsg != WM_RBUTTONUP) {
     return;
   }
 
-	POINT pt;
-	if (GetCursorPos(&pt)) {
-		HMENU menu = CreatePopupMenu();
-		AppendMenu(menu, MF_STRING, 1, "Bot");
-		AppendMenu(menu, MF_STRING, 2, "Quit");
-		UINT cmd = TrackPopupMenu(menu, TPM_RETURNCMD|TPM_RIGHTBUTTON, pt.x, pt.y, 0, g_hMainWnd, NULL);
-		if (cmd == 1) {
-			ShellExecute(NULL, "open", "http://makielski.net/mobileui", NULL, NULL, SW_SHOWNORMAL);
+  POINT pt;
+  if (GetCursorPos(&pt)) {
+    HMENU menu = CreatePopupMenu();
+    AppendMenu(menu, MF_STRING, 1, "Bot");
+    AppendMenu(menu, MF_STRING, 2, "Quit");
+    UINT cmd = TrackPopupMenu(menu, TPM_RETURNCMD|TPM_RIGHTBUTTON, pt.x, pt.y, 0, g_hMainWnd, NULL);
+    if (cmd == 1) {
+      ShellExecute(NULL, "open", "http://makielski.net/mobileui", NULL, NULL, SW_SHOWNORMAL);
     } else if (cmd == 2) {
-			PostMessage(g_hMainWnd, WM_CLOSE, 0, 0);
+      PostMessage(g_hMainWnd, WM_CLOSE, 0, 0);
     }
-	}
+  }
 }
 
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   if (uMsg == WM_DESTROY) {
     s.stop();
     t.join();
-		PostQuitMessage(0);
-		return 0;
+    PostQuitMessage(0);
+    return 0;
   }
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+  return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
-bool CreateMainWnd()
-{
-	static const char CLASS_NAME[] = "Main Class";
+bool CreateMainWnd() {
+  static const char CLASS_NAME[] = "Main Class";
 
-	WNDCLASS wc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hInstance = (HINSTANCE) GetModuleHandle(NULL);
-	wc.lpfnWndProc = &MainWndProc;
-	wc.lpszClassName = CLASS_NAME;
-	wc.lpszMenuName = NULL;
-	wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+  WNDCLASS wc;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  wc.hInstance = (HINSTANCE) GetModuleHandle(NULL);
+  wc.lpfnWndProc = &MainWndProc;
+  wc.lpszClassName = CLASS_NAME;
+  wc.lpszMenuName = NULL;
+  wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
 
-	if (!RegisterClass(&wc))
-		return false;
+  if (!RegisterClass(&wc))
+    return false;
 
-	g_hMainWnd = CreateWindowEx(
-		0,
-		CLASS_NAME,
-		"Main Window",
-		WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
-		CW_USEDEFAULT, CW_USEDEFAULT, 300, 200,
-		NULL,
-		NULL,
-		wc.hInstance,
-		NULL
-	);
+  g_hMainWnd = CreateWindowEx(
+    0,
+    CLASS_NAME,
+    "Main Window",
+    WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+    CW_USEDEFAULT, CW_USEDEFAULT, 300, 200,
+    NULL,
+    NULL,
+    wc.hInstance,
+    NULL
+  );
 
-	return true;
+  return true;
 }
 
 int CALLBACK WinMain(
@@ -81,19 +80,19 @@ int CALLBACK WinMain(
   _In_  LPSTR lpCmdLine,
   _In_  int nCmdShow
 ) {
-	if (!CreateMainWnd())
-		return -1;
+  if (!CreateMainWnd())
+    return -1;
 
   t = std::thread(([]() { s.start(9003); }));
 
-	g_TrayIcon.SetListener(on_action);
+  g_TrayIcon.SetListener(on_action);
   g_TrayIcon.SetVisible(true);
 
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+  MSG msg;
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
 
-	return (int) msg.wParam;
+  return (int) msg.wParam;
 }
