a59265e8cc8c19d26df4ca89c1a28623e7368cb2
deskjet <simon.guendling@gmx.de>
2013-08-03 22:35:51 +0200

    user messages

---
+++ src/messages/user/update/delete_update_msg.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2013, makielski.net
+// Licensed under the MIT license
+// https://raw.github.com/makielski/botscript/master/COPYING
+
+#include "./delete_update_msg.h"
+
+namespace botscript_server {
+
+delete_update_msg::delete_update_msg(const rapidjson::Document& doc)
+    : user_msg(doc),
+      current_pw(doc["arguments"]["current_pw"].GetString()),
+}
+
+
+}  // namespace botscript_server
