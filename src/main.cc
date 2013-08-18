b3d12e587b3786420a7648f42ad7a86003b598dc
Felix Gündling <felix.guendling@gmail.com>
2013-08-18 22:09:39 +0200

    websockets, yeah!

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,16 @@
 #include <iostream>
 
-#include <websocketpp/config/asio_no_tls.hpp>
-#include <websocketpp/server.hpp>
+#include "websocketpp/config/asio_no_tls.hpp"
+#include "websocketpp/server.hpp"
+
+#include "rapidjson_with_exception.h"
+
+#include "./storage/db_config_store.h"
+#include "./storage/db_user_store.h"
+#include "./bot_manager.h"
+#include "./messages/outgoing_msgs.h"
+
+using namespace botscript_server;
 
 typedef websocketpp::server<websocketpp::config::asio> server;
 
@@ -11,32 +20,49 @@ using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
 using websocketpp::lib::ref;
 
-void custom_on_msg(server & s, connection_hdl hdl, server::message_ptr msg) {
-        std::cout << "Message sent to custom handler" << std::endl;
+msg_callback create_cb(server& s, connection_hdl hdl) {
+  return [&s, hdl](std::vector<outgoing_msg_ptr> msgs) {
+    for (const auto& msg : msgs) {
+      s.send(hdl, msg->to_json(), websocketpp::frame::opcode::TEXT);
+    }
+  };
 }
 
-void default_on_msg(server & s, connection_hdl hdl, server::message_ptr msg) {
-    std::cout << "Message sent to default handler" << std::endl;
+void default_on_msg(server& s, bot_manager& mgr, connection_hdl hdl, server::message_ptr msg) {
+  rapidjson::Document d;
+  bool failure = d.Parse<0>(msg->get_payload().c_str()).HasParseError();
+  if (failure) {
+    std::cerr << "could not parse message \"" << msg->get_payload() << "\" "
+              << "(no valid JSON message)\n";
+    return;
+  }
 
-    if (msg->get_payload() == "upgrade") {
-        // Upgrade our connection_hdl to a full connection_ptr
-        server::connection_ptr con = s.get_con_from_hdl(hdl);
-
-        // Change the on message handler for this connection only to 
-        // custom_on_mesage
-        con->set_message_handler(bind(&custom_on_msg,ref(s),::_1,::_2));
-        std::cout << "Upgrading connection to custom handler" << std::endl;
+  try {
+    std::string type = d["type"][rapidjson::SizeType(0)].GetString();
+    if (type == "register") {
+      mgr.handle_register_msg(register_msg(d), create_cb(s, hdl));
+    } else if (type == "login") {
+      mgr.handle_login_msg(login_msg(d), create_cb(s, hdl));
     }
+  } catch(const rapidjson_exception& e) {
+    std::cerr << "could not parse message \"" << msg->get_payload() << "\" "
+              << "(expected attributes missing)\n";
+  }
 }
 
 int main() {
-    server s;
+  boost::asio::io_service io_service;
 
-    s.set_message_handler(bind(&default_on_msg,ref(s),::_1,::_2));
+  db_config_store config_store("./config_store.kch", &io_service);
+  db_user_store user_store("./user_store.kch", &io_service);
+  bot_manager manager(config_store, user_store, &io_service);
 
-    s.init_asio();
-    s.listen(9002);
-    s.start_accept();
+  server s;
+  s.set_message_handler(bind(&default_on_msg, ref(s), ref(manager), ::_1,::_2));
 
-    s.run();
+  s.init_asio(&io_service);
+  s.listen(9003);
+  s.start_accept();
+
+  s.run();
 }
