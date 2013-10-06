cff2703c97a9a65b810122d98b737f30522b3e31
Felix Gündling <felix.guendling@gmail.com>
2013-10-06 22:11:33 +0200

    starting dust integration

--- src/main.cc
+++ src/main.cc
@@ -2,10 +2,11 @@
 // Licensed under the MIT license
 // https://raw.github.com/makielski/botscript/master/COPYING
 
-#include "./ws_server.h"
+#include <iostream>
 
 int main() {
-  botscript_server::ws_server s;
-  s.start("0.0.0.0", "9003");
-  std::cout << "... exit\n";
+//  botscript_server::ws_server s;
+//  s.start("0.0.0.0", "9003");
+//  std::cout << "... exit\n";
+  std::cout << "\n";
 }
