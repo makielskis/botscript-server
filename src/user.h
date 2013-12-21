bc665bb2810ba4448d7df97f0a086ec6df8b3053
Felix Gündling <felix.guendling@gmail.com>
2013-12-21 21:59:42 +0100

    starting to implement user class

--- src/user.h
+++ src/user.h
@@ -9,25 +9,44 @@
 #include <vector>
 #include <memory>
 
-#include "bot_config.h"
-
 #include "dust/document.h"
+
+namespace botscript {
+class bot_config;
+}
 
 namespace botscript_server {
 
+/// The user object provides a API to a persistent user entity.
 class user {
  public:
+  /// Creates a user object from an existing document.
+  /// Throws user_not_found if the document does not contain a valid user.
   user(dust::document doc);
+
+  /// Creates a new peristent user.
+  /// Throws username_already_taken if the user already exists in the database.
+  ///
+  /// \param doc       the document to store this user in
+  /// \param password  the password of this user
   user(dust::document doc,
-       const std::string& username,
        const std::string& password);
 
+  /// \return the username of this user entity
   std::string username();
 
+  /// Sets the new password if the old password was correct, otherwise throws
+  /// an password_wrong error.
+  ///
+  /// \param old_pw  the old password to check (before setting the new)
+  /// \param new_pw  the new password to set (if the old password was correct)
   void password(const std::string& old_pw, const std::string& new_pw);
 
+  /// \param password  the password to check
+  /// \return true if the password did match the users password
   bool check_password(const std::string& password);
 
+  /// \return the users bot configurations
   std::vector<std::shared_ptr<botscript::bot_config>> bots_config();
 
   dust::document bots_document();
