4d80028bfcc9ae561dc1bde5d4bd47c59474d9da
Felix Gündling <felixguendling@gmail.com>
2013-12-21 22:25:11 +0100

    remove username check, remove dbg output

--- test/user_test.cpp
+++ test/user_test.cpp
@@ -14,18 +14,13 @@ using namespace dust;
 using namespace botscript_server;
 
 class user_test: public testing::Test {
-public:
+ public:
   user_test()
       : store_(std::make_shared<mem_store>()),
         users_(store_, "users") {
-    std::cout << "Setup" << std::endl;
   }
 
-  ~user_test() {
-    std::cout << "Shutdown" << std::endl;
-  }
-
-protected:
+ protected:
   std::shared_ptr<key_value_store> store_;
   document users_;
 };
@@ -33,9 +28,6 @@ protected:
 TEST_F(user_test, construct_from_existing_test) {
   users_["test_user"]["password"] = PW_HASH;
 
-  std::cout << "1" << std::endl;
   user u(users_["test_user"]);
-  std::cout << "2" << std::endl;
   ASSERT_EQ("test_user", u.username());
-  std::cout << "3" << std::endl;
 }
