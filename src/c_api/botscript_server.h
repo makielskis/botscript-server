65263f4cd168e69df24e3c6f59e0ed53d894fbe3
Felix Gündling <felixguendling@gmail.com>
2014-01-03 20:07:58 +0100

    c api files

---
+++ src/c_api/botscript_server.h
@@ -0,0 +1,11 @@
+#ifndef BOTSCRIPT_SERVER_H_
+#define BOTSCRIPT_SERVER_H_
+
+extern "C" {
+void* create_server();
+void start_server(void* server);
+void stop_server(void* server);
+void close_server(void* server);
+}
+
+#endif  // BOTSCRIPT_SERVER_H_
