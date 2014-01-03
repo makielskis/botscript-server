c6730103f97998794b2157cc7ba8872c0b7e98a4
Felix Gündling <felixguendling@gmail.com>
2014-01-03 23:31:52 +0100

    c api: db path and packages path relative to executable path

--- src/c_api/botscript_server.cc
+++ src/c_api/botscript_server.cc
@@ -3,6 +3,7 @@
 #include <memory>
 
 #include "boost/thread.hpp"
+#include "boost/filesystem.hpp"
 
 #include "dust/storage/cached_db.h"
 
@@ -14,25 +15,27 @@ namespace bss = botscript_server;
 
 struct server {
 public:
-  server()
+  server(std::string packages_path, const std::string& db_path)
       : s(bss::ws_server_options("127.0.0.1", "9003"),
-          bss::bs_server_options(
-              false, true, "/Users/felix/Code/bot/botscript-server/packages", true),
-          &ios, std::make_shared<dust::cached_db>("/Users/felix/Code/bot/botscript-server/db")) {
+          bss::bs_server_options(false, true, std::move(packages_path), true),
+          &ios, std::make_shared<dust::cached_db>(db_path)) {
   }
-
   boost::asio::io_service ios;
   bss::ws_server s;
   boost::thread t;
 };
 
-void* create_server() {
-  return new server();
+void* create_server(const char* arg0) {
+  using boost::filesystem::path;
+  std::string exe_dir = path(arg0).parent_path().generic_string();
+  std::string packages_path = std::string(exe_dir) + "/../Resources/packages";
+  std::string db_path = std::string(exe_dir) + "/db";
+  return new server(packages_path, db_path);
 }
 
 void start_server(void* data) {
   server* s = reinterpret_cast<server*>(data);
-  s->t = boost::thread([s]() { s->s.start(); });
+  s->t = boost::thread([s]() {s->s.start();});
 }
 
 void close_server(void* data) {
