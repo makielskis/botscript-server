247190c30ec36fc792d469b6cf90a807c37483de
Felix Gündling <felix.guendling@gmail.com>
2014-01-03 23:31:52 +0100

    c api: db path and packages path relative to executable path

--- src/c_api/botscript_server.h
+++ src/c_api/botscript_server.h
@@ -2,7 +2,7 @@
 #define BOTSCRIPT_SERVER_H_
 
 extern "C" {
-void* create_server();
+void* create_server(const char* exe_dir);
 void start_server(void* server);
 void stop_server(void* server);
 void close_server(void* server);
