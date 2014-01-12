5780459542a4230dd594c02b7a10839ff44b5628
Felix Gündling <felix.guendling@gmail.com>
2014-01-12 16:54:43 +0100

    remove close_server from c api, use c api in jni api, don't stop websocketpp server in ws_server

--- src/c_api/botscript_server.h
+++ src/c_api/botscript_server.h
@@ -5,7 +5,6 @@ extern "C" {
 void* create_server(const char* exe_dir);
 void start_server(void* server);
 void stop_server(void* server);
-void close_server(void* server);
 }
 
 #endif  // BOTSCRIPT_SERVER_H_
