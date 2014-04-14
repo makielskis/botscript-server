f508a9c4f6ce2a7721b8b19552aaa51c8dc65bef
Felix Gündling <felixguendling@gmail.com>
2014-04-14 15:03:49 +0200

    android without allowed_users

--- src/allowed_users_parser.h
+++ src/allowed_users_parser.h
@@ -8,12 +8,29 @@
 
 namespace botscript_server {
 
+#ifdef ANDROID
+
+void check_user_allowed(bs_server_options const& opt, std::string const& name) {
+}
+void check_bot_allowed(std::istream& in, std::string const& name,
+                       std::string const& bot) {
+}
+void check_user_allowed(std::istream& in, std::string const& name) {
+}
+void check_bot_allowed(bs_server_options const& opt, std::string const& name,
+                       std::string const& bot) {
+}
+
+#else
+
 void check_user_allowed(bs_server_options const& opt, std::string const& name);
 void check_bot_allowed(std::istream& in, std::string const& name,
                        std::string const& bot);
 void check_user_allowed(std::istream& in, std::string const& name);
 void check_bot_allowed(bs_server_options const& opt, std::string const& name,
                        std::string const& bot);
+
+#endif
 
 }  // namespace botscript_server
 
