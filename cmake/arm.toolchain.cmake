c080d8d1a79460c28cc684469d8c79be890ea2e2
Felix Gündling <felix.guendling@gmail.com>
2014-11-30 20:00:28 +0100

    :home directory variable for toolchain path, arm server-package target with stripped binary in zip file

--- cmake/arm.toolchain.cmake
+++ cmake/arm.toolchain.cmake
@@ -1,15 +1,17 @@
 # CMake toolchain file for building ARM software
 
+SET(HOME_DIR $ENV{HOME})
+
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   ~/x-tools/arm-rpi-linux-gnueabi/bin/arm-rpi-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER ~/x-tools/arm-rpi-linux-gnueabi/bin/arm-rpi-linux-gnueabi-g++)
-SET(CMAKE_STRIP ~/x-tools/arm-rpi-linux-gnueabi/bin/arm-rpi-linux-gnueabi-strip)
+SET(CMAKE_C_COMPILER   ${HOME_DIR}/x-tools/arm-rpi-linux-gnueabi/bin/arm-rpi-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER ${HOME_DIR}/x-tools/arm-rpi-linux-gnueabi/bin/arm-rpi-linux-gnueabi-g++)
+SET(CMAKE_STRIP ${HOME_DIR}/x-tools/arm-rpi-linux-gnueabi/bin/arm-rpi-linux-gnueabi-strip)
 
 # where is the target environment
-SET(CMAKE_FIND_ROOT_PATH  ~/x-tools/arm-rpi-linux-gnueabi/arm-rpi-linux-gnueabi/sysroot)
+SET(CMAKE_FIND_ROOT_PATH  ${HOME_DIR}/x-tools/arm-rpi-linux-gnueabi/arm-rpi-linux-gnueabi/sysroot)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
