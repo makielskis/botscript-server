621adceb7e8ed0fa3f9d5e7fc562c948b3d4454f
Felix Gündling <felix.guendling@gmail.com>
2021-01-19 23:30:37 +0100

    force default visibility

--- src/c_api/botscript_server.h
+++ src/c_api/botscript_server.h
@@ -2,9 +2,9 @@
 #define BOTSCRIPT_SERVER_H_
 
 extern "C" {
-void* create_server(const char* exe_dir);
-void start_server(void* server);
-void stop_server(void* server);
+__attribute__ ((visibility ("default"))) void* create_server(const char* exe_dir);
+__attribute__ ((visibility ("default"))) void start_server(void* server);
+__attribute__ ((visibility ("default"))) void stop_server(void* server);
 }
 
 #endif  // BOTSCRIPT_SERVER_H_
